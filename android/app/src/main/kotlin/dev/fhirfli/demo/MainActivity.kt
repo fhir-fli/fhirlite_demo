c735d86050adaf1610691ba0a177c4339401fe4a
Dokotela <grey.faulkenberry@mayjuun.com>
2022-12-08 16:04:30 -0500

    Use this to test

---
+++ android/app/src/main/kotlin/dev/fhirfli/demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.fhirfli.demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:fhirlite/main.dart';
+import 'package:flutter/material.dart';
+
+/// Will probably have a splash screen or something for loading assets
+void main() => runApp(const FHIRLite());

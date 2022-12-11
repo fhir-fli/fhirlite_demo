9145afd466ca6956b9bc9d23258fe5907ad5429d
Dokotela <grey.faulkenberry@mayjuun.com>
2022-12-10 22:06:44 -0500

    Trying to run

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
-import 'package:fhirlite/main.dart';
+import 'package:fhirlite/fhirlite.dart';
 import 'package:flutter/material.dart';
 
 /// Will probably have a splash screen or something for loading assets
-void main() => runApp(const FHIRLite());
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(home: LoginView(configs));
+}
+
+final configs = <String, dynamic>{
+  'icon': 'assets/images/flames.gif',
+};

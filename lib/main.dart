40861240128750974658555c5e4be013b9100d43
Dokotela <grey.faulkenberry@mayjuun.com>
2022-12-11 22:00:36 -0500

    Basic config files

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,7 @@ import 'package:fhirlite/fhirlite.dart';
 import 'package:flutter/material.dart';
 
 /// Will probably have a splash screen or something for loading assets
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => MaterialApp(home: LoginView(configs));
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(fhirLite('assets/assets.json'));
 }
-
-final configs = <String, dynamic>{
-  'icon': 'assets/images/flames.gif',
-};

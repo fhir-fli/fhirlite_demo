f52e1cb7ead1e4f7bf45b4f3266de289fc7f8779
Dokotela <grey.faulkenberry@mayjuun.com>
2023-05-17 02:39:32 -0400

    trying to minimalize

---
+++ android/app/src/main/kotlin/dev/fhirfli/fhirlite_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.fhirfli.fhirlite_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:fhirlite/fhirlite.dart';
+import 'package:fhirlite/init.dart';
 import 'package:flutter/material.dart';
+
+const configFile = 'assets/config.json';
 
 /// Will probably have a splash screen or something for loading assets
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(fhirLite('assets/assets.yaml'));
+  runApp(const Init('assets/config.json'));
 }

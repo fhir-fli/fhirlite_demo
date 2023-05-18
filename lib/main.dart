f018adea696db5ec4adf50e90878ba1987361771
Dokotela <grey.faulkenberry@mayjuun.com>
2023-05-17 20:24:10 -0400

    Not terrible

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
-import 'package:fhirlite/init.dart';
+import 'dart:convert';
+
+import 'package:fhirlite/fhirlite.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 const configFile = 'assets/config.json';
 
 /// Will probably have a splash screen or something for loading assets
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const Init('assets/config.json'));
+  final Map<String, dynamic> configAssets =
+      jsonDecode(await rootBundle.loadString(configFile))
+          as Map<String, dynamic>;
+  ClientAssets customClientAssets() =>
+      ClientAssets.fromJson(configAssets, GlobalKey<ScaffoldMessengerState>());
+  getIt.registerSingleton<ClientAssets>(customClientAssets());
+  runApp(const ProviderScope(child: Init(configFile)));
 }

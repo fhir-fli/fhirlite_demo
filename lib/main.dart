1f86286f4d6c22c9c1b41f14e055f7ecb3a4be74
Dokotela <grey.faulkenberry@mayjuuun.com>
2023-05-21 10:18:36 -0400

    comments

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+/// This is the path to the config file (currently json, but could be yaml
+/// in the future), contains app information, including app name, links for
+/// thinks like privacy policy, and theming information
 const configFile = 'assets/config.json';
 
-/// Will probably have a splash screen or something for loading assets
+/// Could move this to primary repo for production, but loading ClientAssets
+/// here prevents some errors being throw on refresh during dev
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  /// Read the client assets from file
   final Map<String, dynamic> configAssets =
       jsonDecode(await rootBundle.loadString(configFile))
           as Map<String, dynamic>;
-  ClientAssets customClientAssets() =>
-      ClientAssets.fromJson(configAssets, GlobalKey<ScaffoldMessengerState>());
-  getIt.registerSingleton<ClientAssets>(customClientAssets());
+
+  /// Assign the client assets to the getIt singleton
+  getIt.registerSingleton<ClientAssets>(
+      ClientAssets.fromJson(configAssets, GlobalKey<ScaffoldMessengerState>()));
+
+  /// Ensure you include the Provider scope, and call the Init widget (this
+  /// initializes all of our async things prior to running the app)
   runApp(const ProviderScope(child: Init(configFile)));
 }

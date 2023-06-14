65fdb10d73bb38b9105914742e438cc297713e5d
Dokotela <grey.faulkenberry@mayjuun.com>
2023-06-14 18:13:10 -0400

    Just seeing if there's basic functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:convert';
-
 import 'package:fhirlite/fhirlite.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// This is the path to the config file (currently json, but could be yaml
@@ -14,15 +11,6 @@ const configFile = 'assets/config.json';
 /// here prevents some errors being throw on refresh during dev
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  /// Read the client assets from file
-  final Map<String, dynamic> configAssets =
-      jsonDecode(await rootBundle.loadString(configFile))
-          as Map<String, dynamic>;
-
-  /// Assign the client assets to the getIt singleton
-  getIt.registerSingleton<ClientAssets>(
-      ClientAssets.fromJson(configAssets, GlobalKey<ScaffoldMessengerState>()));
 
   /// Ensure you include the Provider scope, and call the Init widget (this
   /// initializes all of our async things prior to running the app)
